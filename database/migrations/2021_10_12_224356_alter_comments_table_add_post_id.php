106c8a5ee0d3cbe1b3a884c33b54b9f3e81fe446
dilanlongla <44490676+dilanlongla@users.noreply.github.com>
2021-10-13 00:02:58 +0100

    created post_categories and updated comments table in migrations

---
+++ database/migrations/2021_10_12_224356_alter_comments_table_add_post_id.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterCommentsTableAddPostId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->unsignedInteger('post_id');
+            $table->foreign('post_id')->references('id')->on('posts')->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
