cb74125302536161bd9ab143670556bb52e2dac7
dilanlongla <44490676+dilanlongla@users.noreply.github.com>
2021-10-18 08:02:58 +0100

    changes

--- resources/views/layouts/blog_app.blade.php
+++ resources/views/layouts/blog_app.blade.php
@@ -75,8 +75,8 @@
                         <h6 class="text-white">Company Directory</h6>
                     </div>
                     <div class="contact-details">
-                        <p>53 Broadway, Broklyn, NY 11249</p>
-                        <p>Phone: (020) 123 456 789</p>
+                        <p>Poste Central Yaounde</p>
+                        <p>Phone: (237) 656628092</p>
                         <p>Email: <a href="mailto:info@company.com">Info@Company.com</a></p>
                         <ul class="social-menu">
                             <li class="list-inline-item"><a href="#"><i class="fa fa-facebook"></i></a></li>
@@ -87,7 +87,7 @@
                         </ul>
                     </div>
                 </div>
-                <div class="col-md-4">
+                <!-- <div class="col-md-4">
                     <div class="menus d-flex">
                         <ul class="list-unstyled">
                             <li> <a href="#">My Account</a></li>
@@ -102,7 +102,7 @@
                             <li> <a href="#">Contact</a></li>
                         </ul>
                     </div>
-                </div>
+                </div> -->
                 <div class="col-md-4">
                     <div class="latest-posts"><a href="#">
                             <div class="post d-flex align-items-center">
